8b89b4aa4344c86127beda54d6d12c5369d18d54
MahshadOliaie <mahshadoliaie@gmail.com>
2023-09-12 16:58:44 +0330

    using sass

---
+++ assets/js/renders.js
@@ -0,0 +1,49 @@
+
+
+
+function render(data) {
+    let template = data.map(product => {
+        const { title, image, price, category, rating ,id } = product;
+
+        return `<div class="root__card" onclick="products(${id})">
+        <div class="root__card__image">
+            <img src="${image}" alt="">
+        </div>
+    
+        <div class="root__card__about">
+            <h3 class="root__card__about__title">${title}</h3>
+            <p class="root__card__about__category">category: ${category}</p>
+            <p class="root__card__about__rating">rate: ${rating.rate}</p>
+            <p class="root__card__about__price">price: ${price} $</p>
+        </div>
+    </div>`
+    }).join("");
+
+    root.innerHTML = template;
+}
+
+
+
+function showProduct(data) {
+    const { title, image, price, category, rating , description } = data;
+
+    root.innerHTML = `<div class="root__mainImg">
+    <img src="${image}" alt="">
+</div>
+<div class="root__aboutProduct">
+    <h2 class="root__aboutProduct__titleProduct">${title}</h2>
+    <p class="root__aboutProduct__description">${description}</p>
+    <p class="root__aboutProduct__categoryProduct">category: ${category}</p>
+    <p class="root__aboutProduct__ratingProduct">rate: ${rating.rate}</p>
+    <p class="root__aboutProduct__priceProduct">price: ${price} $</p>
+    <button class="root__aboutProduct__shop">ADD</button>
+</div>`;
+
+    root.classList.add("showProduct");
+    root.classList.remove("allProducts");
+
+}
+
+
+
+products();
