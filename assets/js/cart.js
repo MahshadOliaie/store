a751b4be045a3f00f4de500c0aa08fef57eb86cf
MahshadOliaie <mahshadoliaie@gmail.com>
2023-09-23 13:39:08 +0330

    fix back btn

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -19,12 +19,7 @@ function cartsRender(data) {
     let template = data.map(item => {
         const { title, image, price, category, rating } = item;
 
-        return ` <svg xmlns="http://www.w3.org/2000/svg" class="back" height="1em" viewBox="0 0 320 512"
-        onclick="products(null , isFirstPage)"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-        <path
-            d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-    </svg>
-    <div class="carts__cart">
+        return `<div class="carts__cart">
         <div class="carts__cart__image">
             <img src="${image}" alt="">
         </div>
@@ -38,7 +33,11 @@ function cartsRender(data) {
     </div>`
     }).join("");
 
-    root.innerHTML = template;
+    root.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="back" height="1em" viewBox="0 0 320 512"
+    onclick="products(null , isFirstPage)"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+    <path
+        d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+</svg>` + template;
 }
 
 
